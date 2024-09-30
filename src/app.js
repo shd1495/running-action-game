f67915729757827edb413a1c9b02076ed2b7e37b
박지영 <124862122+shd1495@users.noreply.github.com>
2024-09-30 17:53:35 +0900

    feat: "dotenv 추가"
    port 번호를 .env에서 관리하도록 수정

--- src/app.js
+++ src/app.js
@@ -2,11 +2,14 @@ import express from 'express';
 import { createServer } from 'http';
 import initSocket from './init/socket.js';
 import { loadGameAssets } from './init/assets.js';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const PORT = process.env.SECRET_PORT;
 
 const app = express();
 const server = createServer(app);
-
-const PORT = 3030;
 
 // body-parser
 app.use(express.json());
